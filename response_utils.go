1cdbc5b746c0edb65a0c9ec989d6b52329e3b2de
Mahdi Younesi <mehdi.younesi7@gmail.com>
2024-05-10 20:12:01 +0200

    add response utils

---
+++ response_utils.go
@@ -0,0 +1,28 @@
+package atlas
+
+import (
+	"encoding/json"
+	"net/http"
+)
+
+func (at *Atlas) WriteJson(w http.ResponseWriter, status int, data interface{}, headers ...http.Header) error {
+	out, err := json.MarshalIndent(data, "", "\t")
+	if err != nil {
+		return err
+	}
+
+	if len(headers) > 0 {
+		for k, v := range headers[0] {
+			w.Header()[k] = v
+		}
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(status)
+	_, err = w.Write(out)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
